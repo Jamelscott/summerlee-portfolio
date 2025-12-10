7c7eb5b0f8a134acf0e8b58674a9a93e05d4d2f9
Jamel-bbox <jamel@balloonbox.io>
2025-12-09 17:08:10 -0800

    type defs for vite-plugin-svgr/client

--- src/components/ui/shadcn-io/aurora-background/index.tsx
+++ src/components/ui/shadcn-io/aurora-background/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React, { ReactNode } from "react";
+import React from "react";
+import type { ReactNode } from "react";
 import { cn } from "@/lib/utils";
 
 interface AuroraBackgroundProps extends React.HTMLAttributes<HTMLDivElement> {
